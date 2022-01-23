6b9e9b2b614940143a0b747bc73d1945d380557a
Bárbara Villaseñor <barbara.villasenor.c@gmail.com>
2022-01-22 23:47:21 -0300

    Quinto Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ $(".page-header ul a").on("click", function (e) {
     const href = $(this).attr("href");
     $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
   });
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
